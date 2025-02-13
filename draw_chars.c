f94d8c566f33ce0304e310744584e55b5695aec8
The7Doctor <agabrielm77@proton.me>
2025-02-13 09:56:03 -0700

    This is changeing the code

--- draw_chars.c
+++ draw_chars.c
@@ -14,3 +14,33 @@ void print_char_5x7(char c)
   }
   putchar('\n');
 }
+
+void print_char_8x12(char c)
+{
+  c -=0x20;
+  for (char col = 0; col<10; col++){
+    for (char row = 0; row <12; row++){
+      unsigned short rowBits = font_8x12[c][col];
+      unsigned short colMask = 1<< (11-row);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
+
+void print_char_11x16(char c)
+{
+  c -= 0x20;
+  for(char col = 0; col<12; col++){
+    for(char row = 0; row<16; row++){
+      unsigned short rowBits = font_11x16[c][col];
+      unsigned short colMask = 1 << (17-row);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+      
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
+		     
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars5x7 = c, Chars8x12=e, Chars11x16= l , Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -25,9 +25,27 @@ int main()
       print_square(5, 5);
       break;
     case 'c':
-      puts("You selected chars:");
+      puts("You selected 5x7chars:");
       for (char c = 'a'; c < 'd'; c++)
 	print_char_5x7(c);
+      break;
+
+    case 'e':
+      puts("You selected 8x12chars");
+      for(char c='a'; c <'d'; c++)
+	print_char_8x12(c);
+      break;
+
+    case 'l':
+      puts("You selected 11x16chars");
+      for(char c='a';c<'d';c++)
+	print_char_11x16(c);
+      break;
+      
+    case 'a':      
+      puts("You selected arrow");
+      char a = 0x7f;
+      print_arrow(a);
       break;
     case 'q':
       puts("Bye!");
